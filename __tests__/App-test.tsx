d7ed8383b6d5ff7df6b588e6ae837aa3be02bcbe
oleg79 <0112oleg@gmail.com>
2019-04-06 19:21:28 +0300

    ADDED: api configs

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,20 @@
 
 import 'react-native';
 import React from 'react';
+import Config from 'react-native-config'
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('<App /> component', () => {
+  it('Should be rendered correctly', () => {
+    renderer.create(<App />);
+  });
+
+  it('Should get env configs', () => {
+    expect(Config.API_URL).toEqual('http://www.omdbapi.com');
+    expect(Config.API_KEY).toEqual('59057b18');
+  });
 });
+
